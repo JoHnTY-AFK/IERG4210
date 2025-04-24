833cf05f6e010955d82f19251abb4ac4673b993d
LifeHater <johntycss@gmail.com>
2025-04-24 22:24:07 +0800

    Add files via upload

--- hashPasswords.js
+++ hashPasswords.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcrypt');
     const saltRounds = 10;
     const adminPass = await bcrypt.hash('adminpassword', saltRounds);
     const userPass = await bcrypt.hash('userpassword', saltRounds);
+    const sandboxPass = await bcrypt.hash('Fd&5cb4VZ', saltRounds);
     console.log(`Admin: ${adminPass}`);
     console.log(`User: ${userPass}`);
+    console.log(`Sandbox: ${sandboxPass}`);
 })();
